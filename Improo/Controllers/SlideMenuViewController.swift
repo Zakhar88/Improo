2e6505b1870f3ade5ede8a980b7f0f37170bddda
Zakhar88 <3axap88@mail.ru>
2017-05-07 15:48:51 +0300

    add simple lside menu.

---
+++ Improo/Controllers/SlideMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SlideMenuViewController.swift
+//  Improo
+//
+//  Created by 3axap on 07.05.17.
+//  Copyright © 2017 3axap. All rights reserved.
+//
+
+import UIKit
+
+class SlideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
